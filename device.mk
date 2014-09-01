23916e5f7e88f6101c0df27bf66974a2ca0c114b
elelinux <ele.linux@gmail.com>
2014-09-01 08:30:34 +0200

    bring inline with cm
    * add cm.dependencies
    * moving the specific audio settings to each device

--- device.mk
+++ device.mk
@@ -23,3 +23,9 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/klte-common/overlay-gsm
 # Inherit from klte-common
 $(call inherit-product, device/samsung/klte-common/klte-common.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
